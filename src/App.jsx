1a2eb2a51e3ed55a6824d09e44a83b8eeea406a4
Ahmad Saeed <ahmadsaeed@192.168.1.3>
2026-08-07 03:14:37 +0500

    4.3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,6 @@ export default function App() {
   // ============================================================
 
   useEffect(() => {
-    setShowReviewModal(true)
 
     if (myLastOrder != null) {
 
