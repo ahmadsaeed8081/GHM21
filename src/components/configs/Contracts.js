41798cc3d6e3515db7d12e40c8e23c61ab4f7319
Ahmad Saeed <ahmadsaeed@Ahmads-MacBook-Air.local>
2026-08-19 18:55:58 +0500

    8.6 commit

--- src/components/configs/Contracts.js
+++ src/components/configs/Contracts.js
@@ -1,6 +1,6 @@
 
 export const USDT_address="0x1E537F8BD3228d4EfBF20bD837a48a96649A8D5A"
-export const contract_address="0x1cEB7EB25b85378897Ce070d8bb73E4d1ae7b8F3"
+export const contract_address="0x22c644Cc7dA7Df6c72b7348e9D35810C3d5320a6"
 export const school_contract_address="0x8A1148C98E8fB6aE10777CA96Bfd410c91A835D5"
 export const token_abi=[
 	{
